6ce273846c0ae6458a5d5dee3e0c38a032bf4c84
Patrick Arminio <patrick.arminio@gmail.com>
2023-07-18 02:19:29 +0200

    Add fake db

---
+++ app/api/fake_db.py
@@ -0,0 +1,33 @@
+from typing import Dict, Optional, TypedDict
+
+
+class Answer(TypedDict):
+    id: int
+    text: str
+    votes: int
+
+
+class Poll(TypedDict):
+    id: int
+    title: str
+    answers: list[Answer]
+    total_votes: int
+
+
+POLLS: Dict[str, Poll] = {
+    "1": {
+        "id": 1,
+        "title": "What's your favourite color?",
+        "answers": [
+            {"id": 1, "text": "Red", "votes": 15},
+            {"id": 2, "text": "Blue", "votes": 7},
+            {"id": 3, "text": "Green", "votes": 3},
+        ],
+        "total_votes": 25,
+    },
+}
+
+
+class Db:
+    def get_poll(self, id: str) -> Optional[Poll]:
+        return POLLS.get(id)
